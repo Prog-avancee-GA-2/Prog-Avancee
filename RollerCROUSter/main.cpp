aba08732b8c8db02bdc33b396a466fd147a01d58
Marion Valls <91269928+Marion-Valls@users.noreply.github.com>
2023-10-18 15:57:22 +0200

    (doc): adding comments

--- RollerCROUSter/main.cpp
+++ RollerCROUSter/main.cpp
@@ -6,81 +6,107 @@
 
 using namespace std;
 
-mutex mut1, mut2;
+// Mutex to protect trail and student
+mutex mut1, mut2, mut3, mut4;
+
 counting_semaphore boardQueue{0}, unboardQueue{0}, crousFull{0}, crousEmpty{0};
+
+//number of trays taken
 int TRAYS_TAKEN = 0;
+//number of trays left
 int TRAYS_LEFT = 0;
 
-const int NB_TRAYS= 20;
-const int NB_STUDENTS=100;
+//max number of trays
+const int NB_TRAYS = 20;
+//max number of students
+const int NB_STUDENTS = 100;
 
-void lunch(){
+void lunch() {
     while (true) {
-            for (int i = 0; i < NB_TRAYS; ++i) {
-                boardQueue.release();
-            }
-
-            for (int i = 0; i < NB_TRAYS; ++i) {
-                crousFull.acquire();
-            }
-
-
-            cout << "Lunch with " << NB_TRAYS << " student's trays." << endl;
-            this_thread::sleep_for(chrono::seconds(2));
-
-            for (int i = 0; i < NB_TRAYS; ++i) {
-                unboardQueue.release();
-            }
-
-            for (int i = 0; i < NB_TRAYS; ++i) {
-                crousEmpty.acquire();
-            }
+        // Fill trays for students
+        for (int i = 0; i < NB_TRAYS; ++i) {
+            boardQueue.release(); // Increase the number of available trays
         }
 
+        // Wait for all trays to be taken by students
+        for (int i = 0; i < NB_TRAYS; ++i) {
+            crousFull.acquire(); // Decrease the number of available trays
+        }
+
+        cout << "Lunch with " << NB_TRAYS << " student's trays." << endl;
+        this_thread::sleep_for(chrono::seconds(2));
+
+        // Students return their trays after eating
+        for (int i = 0; i < NB_TRAYS; ++i) {
+            unboardQueue.release(); // Increase the number of empty trays
+        }
+
+        // Wait for all trays to be empty
+        for (int i = 0; i < NB_TRAYS; ++i) {
+            crousEmpty.acquire(); // Decrease the number of empty trays
+        }
+    }
 }
 
-void student(int id){
-    boardQueue.acquire();
-
-        {
-            lock_guard<mutex> lock(mut1);
-            cout << "Student " << id << " take a tray." << endl;
-            TRAYS_TAKEN += 1;
-            if (TRAYS_TAKEN == NB_TRAYS) {
-                for (int i = 0; i < NB_TRAYS; ++i) {
-                    crousFull.release();
-                }
-                TRAYS_TAKEN = 0;
-            }
-        }
-
-        unboardQueue.acquire();
-
-        {
-            lock_guard<mutex> lock(mut2);
-            cout << "Student " << id << " leave his tray." << endl;
-            TRAYS_LEFT += 1;
-            if (TRAYS_LEFT == NB_TRAYS) {
-                for (int i = 0; i < NB_TRAYS; ++i) {
-                    crousEmpty.release();
-                }
-                TRAYS_LEFT = 0;
-            }
-        }
-}
-
-
-int main()
-{
-    thread car_thread(lunch);
-
-    vector<thread> passenger_threads;
-    for (int i = 1; i <= NB_STUDENTS; ++i) {
-        passenger_threads.emplace_back(student,i);
+void student(int id) {
+    // Wait for an available tray
+    {
+        lock_guard<mutex> lock(mut3);
+        boardQueue.acquire();
     }
 
-    car_thread.join();
-    for (auto &thread : passenger_threads) {
+    // Take a tray
+    {
+        lock_guard<mutex> lock(mut1);
+        cout << "Student " << id << " takes a tray." << endl;
+        TRAYS_TAKEN += 1;
+
+        // When all students have taken a tray, indicate that the restaurant is full
+        if (TRAYS_TAKEN == NB_TRAYS) {
+            for (int i = 0; i < NB_TRAYS; ++i) {
+                crousFull.release(); // Increase the number of full trays
+            }
+            TRAYS_TAKEN = 0;
+        }
+    }
+
+    // Wait for returning the tray after eating
+    {
+        lock_guard<mutex> lock(mut4);
+        unboardQueue.acquire();
+    }
+
+    // Return the tray
+    {
+        lock_guard<mutex> lock(mut2);
+        cout << "Student " << id << " leaves their tray." << endl;
+        TRAYS_LEFT += 1;
+
+        // When all students have returned their trays, indicate that the restaurant is empty
+        if (TRAYS_LEFT == NB_TRAYS) {
+            for (int i = 0; i < NB_TRAYS; ++i) {
+                crousEmpty.release(); // Increase the number of empty trays
+            }
+            TRAYS_LEFT = 0;
+        }
+    }
+}
+
+int main() {
+    // Thread to simulate the university restaurant
+    thread crous_thread(lunch);
+
+    // Threads to simulate student behavior
+    vector<thread> student_threads;
+    for (int i = 1; i <= NB_STUDENTS; ++i) {
+        student_threads.emplace_back(student, i);
+    }
+
+    // Wait for the university restaurant thread to finish
+    crous_thread.join();
+
+    // Wait for all student threads to finish
+    for (auto &thread : student_threads) {
         thread.join();
     }
 
